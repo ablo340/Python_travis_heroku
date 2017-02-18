1e1e75ed6dc22d35b5b478c3079b6f3934402ffc
Abdoulaye Koolwx <ablo110@live.fr>
2017-02-18 19:57:52 +0100

    Fix utils module to pass test

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 2, 2016
+from math import sqrt
 
 def fact(n):
     """Computes the factorial of a natural number.
@@ -9,7 +10,10 @@ def fact(n):
     Post: Returns the factorial of 'n'.
     Throws: ValueError if n < 0
     """
-
+    if n>=0:
+        for elem in range(1,n):
+            n*=elem
+        return n
 def roots(a, b, c):
     """Computes the roots of the ax^2 + bx + x = 0 polynomial.
     
@@ -17,7 +21,14 @@ def roots(a, b, c):
     Post: Returns a tuple with zero, one or two elements corresponding
           to the roots of the ax^2 + bx + c polynomial.
     """
-    pass
+    delta=(b**2)-(4*a*c)
+    if delta <0:
+        return ()
+    if delta ==0:
+        return -b/(2*a)
+    if delta >0:
+        return (-b+sqrt(delta))/(2*a),(-b-sqrt(delta))/(2*a)
+
 
 def integrate(function, lower, upper):
     """Approximates the integral of a fonction between two bounds
@@ -28,9 +39,14 @@ def integrate(function, lower, upper):
     Post: Returns an approximation of the integral from 'lower' to 'upper'
           of the specified 'function'.
     """
-    pass
+    x = lower
+    sum = 0
+    while x <= upper:
+        sum += eval(function) * 0.01
+        x += 0.01
+    return sum
 
 if __name__ == '__main__':
     print(fact(5))
     print(roots(1, 0, 1))
-    print(integrate('x ** 2 - 1', -1, 1))
+    print(integrate('(x**2) - 1', -1, 1))
